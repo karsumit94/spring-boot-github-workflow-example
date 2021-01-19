8aa5955cb69624b7583b9dd98d08af5ff561eec5
Sumit Kar <sumkar@cisco.com>
2021-01-19 11:12:06 +0530

    Added a dummy test case

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 class DemoApplicationTests {
-
+	@Autowired
+	private HelloRestController controller;
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
